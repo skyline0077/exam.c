48b58079e77e06794a7e689b635c450c99488543
skyline0077 <120244473+skyline0077@users.noreply.github.com>
2022-12-16 17:30:08 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+main(){
+	
+	int i,j,k;
+	
+	for(i=5; i>=1; i--){
+	
+		
+		for(j=1; j<=i; j++){
+			
+			printf("%i ",j);
+			
+			}
+			
+		for(k=5; k>i; k--){
+			
+			printf("    ");
+		}	
+			
+		for(j=i; j>=1; j--){
+			
+			printf("%i ",j);
+			
+			}	
+	
+	printf("\n");
+}
+
+
+}
